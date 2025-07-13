c324fa57f85ae5f4b2df5000ec04d897d0f6f37b
tooj <tjnyaina@gmail.com>
2025-07-13 18:29:26 +0300

    [add] - nvim: telescop ui select

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -19,7 +19,7 @@ return {
       local config = require("lspconfig")
 
       config.lua_ls.setup({})
-      
+
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
