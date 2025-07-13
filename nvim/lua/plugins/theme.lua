bb5b495263b0b926d2bf8d0034d4664731dab249
tooj <tjnyaina@gmail.com>
2025-07-13 13:23:38 +0300

    [patch] - nvim: theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -2,6 +2,10 @@ return {
   { 
     "catppuccin/nvim", 
     name = "catppuccin", 
-    priority = 1000 
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup()
+        vim.cmd.colorscheme("catppuccin")
+    end
   }
 }
