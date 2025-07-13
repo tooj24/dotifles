97a704648bbea59a568c551cdf6d34fda8e05cfa
tooj <tjnyaina@gmail.com>
2025-07-13 10:11:15 +0300

    [add] - options & keymaps

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<leader>wh", ":split<CR>", { desc = "Horizontal Split", silent = true })
+vim.keymap.set("n", "<leader>wv", ":vsplit<CR>", { desc = "Vertical Split", silent = true })
+vim.keymap.set("n", "<leader>wd", ":close<CR>", { desc = "Close Pane", silent = true })
