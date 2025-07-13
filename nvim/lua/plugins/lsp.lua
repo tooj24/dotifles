a7f7563810b27fbbc12753e42c77610606d52670
tooj <tjnyaina@gmail.com>
2025-07-13 18:16:44 +0300

    [add] - nvim: LSP

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local config = require("lspconfig")
+
+      config.lua_ls.setup({})
+      
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end
+  }
+}
